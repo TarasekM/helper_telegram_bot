ec3322d4d4db35141ce0e19605f0525874869af4
TarasekM <rezkth@gmail.com>
2018-10-20 18:21:15 +0200

    Can setup new_event now. TODO calculate time to proc alarm

--- bot_organizer.py
+++ bot_organizer.py
@@ -9,18 +9,21 @@ TOKEN_FILENAME = 'TOKEN.txt'
 
 def read_token(filename):
     with open(filename,'r') as file:
-        token = file.readline()[:-1]
+        token = file.readline()
         return token
 
 def start(bot, update):
     update.message.reply_text('Hi! I\'m orginizer helper bot!')
     
 def help(bot, update):
-    update.message.reply_text('Currently you can use only this commands:\n\
-                              /set <seconds> <timer_name>(optional) - to set timer.\n\
-                              /new_event <hour "HH:MI:SS"> <date "DD-MM-YYYY"> \
-                              <event_name> - to create an new event')
+    update.message.reply_text('Currently you can use only this commands:\n'
+                             '/set <seconds> <timer_name>(optional) - to set timer.\n'
+                             '/new_event <hour "HH:MI:SS"> <date "YYYY-MM-DD">'
+                             '<event_name> - to create an new event')
 
+    #update.message.reply_text('Currently you can use only this commands:\
+    #                         \n/set <seconds> <timer_name>(optional) - to set timer.')
+    
 def alarm(bot, job):
     """Send the alarm message."""
     chat_id = job.context[0]
@@ -60,21 +63,41 @@ def new_event(bot, update, args, job_queue, chat_data):
     """Add a job to the queue."""
     chat_id = update.message.chat_id
     try:
+        time = args[0]
+        date = args[1]
+        event_name = ' '.join(args[2:])
+        time_now = str(datetime.now().time().replace(microsecond=0))
+        date_now = str(datetime.now().date())
         
-        hour = args[0]
-        time_now = datetime.now().time()
-        print(time_now)
+        if time_now > time or date_now > date:
+            update.message.reply_text('Sorry we can not go back to future!')
+            return
+        
+        if name in chat_data:
+            update.message.reply_text(f'Updating \'{event_name}\' event')
+            event = chat_data[event_name]
+            event.schedule_removal()
+        event = job_queue.run_once(event_alarm, time_difference, context=[chat_id, timer_name])
+        chat_data[event_name] = event
+        update.message.reply_text(f'Event {event_name} successfully set!')
 
-        if timer_name in chat_data:
-            update.message.reply_text(f'Updating \'{timer_name}\' timer')
-            timer = chat_data[timer_name]
-            timer.schedule_removal()
-        timer = job_queue.run_once(alarm, due, context=[chat_id, timer_name])
-        chat_data[timer_name] = timer
-        update.message.reply_text(f'Timer \'{timer_name}\' successfully set!')
+    except IndexError:
+        update.message.reply_text('Usage: /new_event <hour "HH:MI:SS"> <date "YYYY-MM-DD">'
+                        '<event_name>')
 
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /set <seconds> <timer_name>(optional)')
+def event_alarm(bot, job):
+    """Send the alarm message."""
+    #TODO
+    chat_id = job.context[0]
+    try:
+        event_name = job.context[1]
+        bot.send_message(chat_id, text=f'Beep: {event_name}!')
+    except IndexError:
+        bot.send_message(chat_id, text='Beep!')
+
+def time_difference(time_now, time_future):
+    #TODO
+    return 0
 
 
 def unset(bot, update, args, chat_data):
